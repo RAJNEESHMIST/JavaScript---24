6f9b54a936314202dfbc4a74677fae6a35356aab
RAJNEESHMIST <rajneeshvermarv299@gmail.com>
2024-07-24 15:40:21 +0000

    new file added

---
+++ 02_basics/01_arrayBasics.js
@@ -0,0 +1,19 @@
+// array
+
+// creation of an array
+// const myArray = [1,2,7,4,true,23.34];
+//  NOTE - we can put any type of data in the single 
+
+const arr = ["Rajneesh ","Anshu","saini","verma"];
+
+// another decalaration
+const arr2 = new Array(45,56,23,52);
+// console.log(arr2[3]);
+// console.log(arr2[10]);
+
+// array methods
+// 1.push(ele)   - this method helps us to insert new element in the array
+
+arr2.push(65);
+arr2.push(98);
+console.log(arr2);
