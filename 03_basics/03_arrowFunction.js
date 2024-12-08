90e29de834c3c1c9edde695889f04681de713fc4
Rajneesh <rajneeshvermarv299@gmail.com>
2024-12-08 15:48:37 +0000

    new

--- 03_basics/03_arrowFunction.js
+++ 03_basics/03_arrowFunction.js
@@ -2,8 +2,8 @@ const user = {
     username:"Rajneesh",
     email: "rajneesh@google.com",
     greet : function(){
-        console.log(`${this.username} , Welcome.`)
-        // console.log(this)
+        console.log(`${this.username} , Welcome to JavaScript free course.`)
+        console.log(this)
     }
 } 
 
@@ -11,4 +11,43 @@ const user = {
 // user.username = "Anshu"
 // user.greet()
 
-console.log(this)
+// console.log(this)
+
+// function chai(){
+//     let uname = "Rajneesh"
+//     // console.log(this) //it will print many values that we will elaborate in dom 
+//     console.log(this.uname);
+// }
+
+// chai();
+
+// another way for funtion declaration
+// const chai = function(){
+//     let uname = "Rajneesh"
+//         console.log(this) //it will print many values that we will elaborate in dom 
+//         console.log(this.uname);
+// }
+// chai()
+
+// Note - it will not work with function as like work with objects
+
+
+// arrow function
+
+const verma = () => {
+    let username = "Rajneesh"
+    console.log(this.username)
+}
+verma();
+
+const addTwo = (num1, num2) => {
+    return num1 + num2;
+}
+
+console.log(addTwo(45,98)) 
+
+// const multi = (a,b) => a * b //this is the also valid synatax of arrow function
+
+const multi = (a,b) => ({ user : "Rajneesh verma"}) //if we want to return an object the we use syntax like this 
+
+console.log(multi(6,9))
