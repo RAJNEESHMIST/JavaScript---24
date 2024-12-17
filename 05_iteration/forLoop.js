aa303e61205a91fe8f6efea89e608ddf69379124
Rajneesh <rajneeshvermarv299@gmail.com>
2024-12-17 06:22:51 +0000

    loop

---
+++ 05_iteration/forLoop.js
@@ -0,0 +1,12 @@
+//for loop - this is the basic loop 
+//syntax 
+//for(initialization;condition;increment/decrement){}
+
+// for example
+for(let i = 0;i <= 10; i++){
+    
+    let ele = i;
+    if(ele == 5) console.log("5 is the best Number");
+
+    console.log(ele);
+}
