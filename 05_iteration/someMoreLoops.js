5da7095aaeb9e706b661c3a5184567a5ba091ac0
Rajneesh <rajneeshvermarv299@gmail.com>
2024-12-18 12:36:58 +0000

    while,do-while, for of loop

---
+++ 05_iteration/someMoreLoops.js
@@ -0,0 +1,49 @@
+//some advance loop used in modern programming
+// 1. for of
+// syantax
+// for (const element of object) {
+    
+// }
+
+//[" "," "," "] - array of string
+//[{}, {},{}] - array of object -> first iterate array then we access element of object using dot(.) notation or square [] notation
+
+// let arr = [12,34,23,56,77,88];
+// for (const element of arr) {
+//     console.log(`The element of array ${element}`);
+      
+// }
+
+// const str = "Rajneesh"
+
+// for (const element of str) {
+//     console.log(`char of string ${element}`);
+// }
+
+//maps -> store the value in the form of key value pairs
+
+const map = new Map(); //syntax to declare a map
+map.set("In" ,"India");
+map.set("Bh" ,"India");
+map.set("Eng" ,"England");
+map.set("Rus" ,"Russia");
+map.set("Aus" ,"Australia");
+map.set("I" ,89);
+map.set(2 ,"India");
+
+//note - it also maintain the order 
+//keys only be unique 
+// console.log(map);
+
+// for (const keyOfMap of map) {
+//     console.log(keyOfMap); // this syntax print the map in the form of key value pair like as -
+// // [ 'In', 'India' ]
+// // [ 'Bh', 'India' ]
+// }
+
+for (const [key,val] of map) {
+    console.log(`${key} :- ${val}`); //in this values are print only key value like this-
+    // In :- India
+    // Bh :- India
+}
+
---
+++ 05_iteration/while_do_while.js
@@ -0,0 +1,26 @@
+// this program is dedicated to understand the concept of while and do-while loop
+
+// let num = 0;
+// while (num <= 10) {
+//     console.log(`the value is ${num}`);
+//     num += 2;
+// }
+
+
+// let myArray = ["Radhey Shyam Verma","Geeta Devi","Vijay Laxmi","Anuradha Verma","Sangam Verma","Rajneesh Verma",
+//     "Anamika Verma"
+// ];
+
+// let i = 0;
+// while(i < myArray.length){
+//     console.log(myArray[i]);
+//     i++;
+    
+// }
+
+let run = 1;
+do{
+    console.log(`The score is ${run}`);
+    run++;
+    
+}while(run <= 5);
