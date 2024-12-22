6ddc049aae22cec522818519be21e98da5c5dae5
Rajneesh <rajneeshvermarv299@gmail.com>
2024-12-22 08:45:56 +0000

    foreach loop

---
+++ 05_iteration/for_each loop.js
@@ -0,0 +1,21 @@
+const subject = ['java','c++','DAA','DSA','python','Coa']
+
+// subject.forEach( function (s){
+//     console.log(s);
+// })
+
+// subject.forEach((i) => {
+//     console.log(i);
+// })
+
+//we can also define a function and pass that function as parameter in for each
+
+function print(item){
+    console.log(item);
+}
+
+// subject.forEach(print);//this also works 
+
+subject.forEach((val,index, arr) => { //we can also access these things along with this using for  each loop
+    console.log(val,index,arr);
+})
